376850a3432bd8ba008d8f05b60be8efc3a54b64
Connor <connornusser@gmail.com>
2023-01-24 14:24:24 -0600

    add more code changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+@Injectable()
+class membersInfo implements OnInit{
+  @Input() Number: number;
+  @Input() Name: string;
+
+  ngOnInit(): void {
+
+    // You can use this value now
+    this.Number = 0;
+    this.Name = "";
+
+}
+
+  constructor(code?: number, name?: string) {
+          if(code && name){
+            this.Name = name;
+            this.Number = code;
+          }else{
+            this.Number = 0;
+            this.Name = "";
+          }
+ 
+  }
+
+  getSalary() : number {
+      return 10000;
+  }
+}
 export class AppComponent {
-  title = 'App1';
+  title = 'Home Page';
   resToAdd = '';
-  members: string[]=[];
+  resNumber = 0;
+  members: membersInfo[]=[];
 
   addMember(){
-    this.members.push(this.resToAdd);
+    let newMember = new membersInfo(this.resNumber, this.resToAdd);
+    this.members.push(newMember);
     console.log(this.members);
+    this.resNumber = this.resNumber + 1;
   }
   onInput(resName: string){
     this.resToAdd = resName;
     console.log(resName);
   }
+
   resetMembers(){
     this.members = [];
     console.log(this.members);
